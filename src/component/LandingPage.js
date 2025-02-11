9121d97c3104ad7b94a8118f292a3c6c95342fdd
sungsieon <mari9908@naver.com>
2025-02-11 16:35:30 +0900

    style: UFO,light반응형수정

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -9,12 +9,13 @@ export default function LandingPage() {
 
       setTimeout(() => {
          setIsTriangle(true);
-      }, 100);
+      }, 2800);
+
    },[]);
 
   return (
     <div
-      className="relative w-full min-h-screen bg-cover z-20"
+      className="relative w-full min-h-screen bg-cover z-20 "
       style={{
         backgroundImage: "url('/img/우주.png')",
         backgroundSize: "cover", // 배경이 꽉 차게 설정
@@ -22,12 +23,13 @@ export default function LandingPage() {
         backgroundAttachment: "fixed", // 배경을 고정
       }}
  >
-   <div className="absolute top-[-35%] left-[-35%] z-10">
-   <img className="rotate-[40deg] w-[100rem] h-[100rem] object-contain animate-move-rotate" src="/img/UFO1.png" />
+   <div className="absolute top-[0%] left-[-34.2%] z-10 w-full h-full ">
+   <img className="rotate-[40deg] w-[83.5vw] h-[83.5vw] max-w-full max-h-full object-contain animate-move-rotate" src="/img/UFO1.png" />
    </div>
    
-   <div className="absolute top-[10%] left-[28.2%] w-0 h-0 border-l-[400px] border-l-transparent border-r-[400px] border-r-transparent border-b-[870px] border-b-[#FFFA72]"/>
-      
+   <div className="absolute top-[10%] left-[35%] max-[1750px]:left-[34.6%] w-full h-full">
+   <div className={isTriangle ? "w-0 h-0 border-l-[15vw] border-l-transparent border-r-[15vw] border-r-transparent max-[2000px]:border-b-[105vh]  max-[1750px]:border-b-[85vh]  max-[640px]:border-b-[85vh]  border-b-[#FFFA72] absolute bottom-0 transition-opacity duration-[4000ms] opacity-100" : "opacity-0"}/>
+   </div>  
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,10 @@ module.exports = {
       keyframes: {
         moveRotate: {
           '0%': {transform: 'translateX(0) rotate(40deg) scale(1)'},
-          '40%':{transform: 'translateX(105rem) rotate(-15deg) scale(0.4)'},
-          '70%':{transform: 'translateX(5rem) rotate(20deg) scale(0.3)  '},
-          '90%':{transform: 'translateX(55rem) translateY(-20rem) rotate(-20deg) scale(0.25)'},
-          '100%':{transform: 'translateX(50rem) translateY(-20rem) rotate(0deg) scale(0.25)'},
+          '40%':{transform: 'translateX(88vw) rotate(-15deg) scale(0.4)'},
+          '70%':{transform: 'translateX(5vw) rotate(20deg) scale(0.3)  '},
+          '90%':{transform: 'translateX(55vw) translateY(-18vw) rotate(-20deg) scale(0.25)'},
+          '100%':{transform: 'translateX(42vw) translateY(-18vw) rotate(0deg) scale(0.25)'},
         },
       },
     },
