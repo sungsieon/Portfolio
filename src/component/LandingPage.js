9bd36490ada25c82a82527cf0e6980396ec392af
sungsieon <mari9908@naver.com>
2025-02-23 23:34:08 +0900

    style: star img추가 및 animation 추가

--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -22,22 +22,44 @@ export default function LandingPage() {
   }, []);
 
   return (
+    // --------우주 배경---------
     <div
       className="relative w-full min-h-screen bg-cover z-20 "
       style={{
         backgroundImage: "url('/img/우주.png')",
-        backgroundSize: "cover", // 배경이 꽉 차게 설정
-        backgroundRepeat: "no-repeat", // 중복 방지
-        backgroundAttachment: "fixed", // 배경을 고정
+        backgroundSize: "cover", 
+        backgroundRepeat: "no-repeat", 
+        backgroundAttachment: "fixed", 
       }}
     >
+
+      
+
       <div className="absolute top-[0%] left-[-34.2%] z-10 w-full h-full ">
         <img
           className="rotate-[40deg] w-[83.5vw] h-[83.5vw] max-w-full max-h-full object-contain animate-move-rotate"
           src="/img/우주2.png"
         />
       </div>
+     {/* ------------별 4개------------ */}
+      <div className="absolute top-1/2 left-[46.2%] w-full z-40">
+      <img className="w-[7vw] z-40" src="/img/star1.png"/>
+      </div>
 
+      <div className="absolute top-1/2 left-[46.2%] w-full z-40">
+      <img className="w-[7vw] z-40" src="/img/star1.png"/>
+      </div>
+
+      <div className="absolute top-1/2 left-[46.2%] w-full z-40">
+      <img className="w-[7vw] z-40" src="/img/star2.png"/>
+      </div>
+
+      <div className="absolute top-1/2 left-[46.2%] w-full z-40">
+      <img className="w-[7vw] z-40" src="/img/star2.png"/>
+      </div>
+
+      {/* ------------------------------ */}
+      {/* ---------우주 레이저----------- */}
       <div className="absolute top-[10%] left-[35%] max-[1750px]:left-[34.6%] w-full h-full">
         <div
           className={
