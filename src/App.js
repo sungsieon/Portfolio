560c89ff9b3a55f1e253336f904d4c150f2bccd7
sungsieon <mari9908@naver.com>
2025-03-04 08:19:26 +0900

    feat: Skill Page 생성 및 LandingPage 수정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import LandingPage from './component/LandingPage';
 import Introduction from './component/Introduction';
+import Skill from './component/Skill';
 
 function App() {
   return (
     <>
     <LandingPage />
     <Introduction />
+    <Skill />
     </>
   );
 }
---
+++ src/component/Skill.js
@@ -0,0 +1,24 @@
+import { useState, useEffect, useRef } from "react";
+
+
+
+
+
+export default function Skill(){
+     
+    return(
+        <>
+        <div
+      className="relative w-full min-h-screen bg-cover z-20 overflow-hidden "
+      style={{
+        backgroundImage: "url('/img/우주.png')",
+        backgroundSize: "cover", 
+        backgroundRepeat: "no-repeat", 
+        backgroundAttachment: "fixed", 
+      }}
+    >
+        
+    </div>
+        </>
+    )
+}
