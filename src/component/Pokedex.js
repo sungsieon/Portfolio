19e92213f5c46dd0bd06a46e4f200886416ab524
sungsieon <mari9908@naver.com>
2025-03-16 13:55:08 +0900

    fix: github 주소 수정

--- src/component/Pokedex.js
+++ src/component/Pokedex.js
@@ -28,7 +28,7 @@ export default function Pokedex(){
 
 <div className="w-full flex justify-between items-center ">
 <div onClick={goHome} className="w-[3rem] fill-[#9EDE52]"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg></div>
-<a><div className="border-2 rounded-[20px] border-[#9EDE52] p-2 cursor-pointer"><span className="text-[#9EDE52]">GitHub</span></div></a>
+<a href="https://github.com/sungsieon"><div className="border-2 rounded-[20px] border-[#9EDE52] p-2 cursor-pointer"><span className="text-[#9EDE52]">GitHub</span></div></a>
 </div>
 
 
