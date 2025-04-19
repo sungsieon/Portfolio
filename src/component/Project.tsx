a5ad926d5068f641741149cb72b1d0e79c1ef9d7
sungsieon <mari9908@naver.com>
2025-04-19 17:51:40 +0900

    style: project컴포넌트 반응형UI 수정

--- src/component/Project.tsx
+++ src/component/Project.tsx
@@ -18,7 +18,7 @@ export default function Project() {
     }
 
     if (typeof window !== "undefined") {
-      setIsMobile(window.innerWidth <= 430);
+      setIsMobile(window.innerWidth <= 1073);
     }
   }, []);
 
@@ -76,7 +76,7 @@ export default function Project() {
           />
         </div>
 
-        <div className="ml-[2vw]  w-[25vw] ">
+        <div className="ml-[2vw]  w-[30vw] ">
           <h1 className="max-[1070px]:hidden text-[3rem]">FoodFinder</h1>
           <p className="max-[1070px]:hidden text-[1.2rem]">
             2024.12.26 ~ 2025.1.12
@@ -86,18 +86,20 @@ export default function Project() {
             FoodFinder는 카카오지도를 통해 부산 맛집을 쉽고 빠르게 검색할 수
             있는 웹 애플리케이션입니다.
           </p>
-        </div>
-      </div>
 
-      <span
+          <span
         onClick={goToFoodFinder}
-        className="absolute top-[29vw] left-[51.6vw] max-[1070px]:hidden text-[#9EDE52] border-2 border-[#9EDE52] p-1 rounded-[15px] "
+        className="max-[1070px]:hidden text-[#9EDE52] border-2 border-[#9EDE52] p-1 rounded-[15px] "
       >
         상세내용
       </span>
+        </div>
+      </div>
+
+      
 
       <div className="flex max-[541px]:ml-0 ml-[24vw] items-center">
-        <div className="mr-[2vw]  w-[25vw] ">
+        <div className="mr-[2vw]  w-[30vw] ">
           <h1 className="max-[1070px]:hidden text-[3rem]">Pokedex</h1>
           <p className="max-[1070px]:hidden text-[1.2rem]">
             2024.11.24 ~ 2024.11.30
@@ -108,6 +110,15 @@ export default function Project() {
             애플리케이션입니다. 포켓몬의 타입, 설명 등을 확인할 수 있으며,
             사용자 친화적인 UI를 제공합니다.
           </p>
+
+         <div className="w-full flex justify-end">
+          <span
+        onClick={goToPokedex}
+        className="max-[1070px]:hidden text-[#9EDE52] border-2 border-[#9EDE52] p-1 rounded-[15px]  "
+      >
+        상세내용
+      </span>
+      </div>
         </div>
 
         <div
@@ -124,14 +135,9 @@ export default function Project() {
           />
         </div>
       </div>
-      <span
-        onClick={goToPokedex}
-        className="absolute top-[62.5vw] left-[45.3vw] max-[1070px]:hidden text-[#9EDE52] border-2 border-[#9EDE52] p-1 rounded-[15px]  "
-      >
-        상세내용
-      </span>
+     
 
-      <div className="flex mt-[10vw] max-[854px]:mt-[10vw] max-[462px]:mt-[10vw] h-full">
+      <div className="flex mt-[10vw] max-[854px]:mt-[10vw] max-[462px]:mt-[10vw] h-full pb-[3rem]">
         <div
           onClick={(): void => {
             if (isMobile) {
@@ -146,7 +152,7 @@ export default function Project() {
           />
         </div>
 
-        <div className="ml-[2vw]  w-[25vw] ">
+        <div className="ml-[2vw]  w-[30vw] ">
           <h1 className="max-[1070px]:hidden text-[3rem]">Web Portfolio</h1>
           <p className="max-[1070px]:hidden text-[1.2rem]">
             2025.2.1 ~ 2025.2.25
@@ -156,15 +162,18 @@ export default function Project() {
             Wep Portfolio는 제 소개와 쓸 수 있는 기술,프로젝트 등이 정리되어
             있습니다.
           </p>
-        </div>
-      </div>
 
-      <span
+        <span
         onClick={goToWebPortfolio}
-        className="absolute top-[93vw] left-[51.6vw] max-[1070px]:hidden text-[#9EDE52] border-2 border-[#9EDE52] p-1 rounded-[15px] "
+        className="max-[1070px]:hidden text-[#9EDE52] border-2 border-[#9EDE52] p-1 rounded-[15px] "
       >
         상세내용
-      </span>
+      </span> 
+        </div>
+       <br></br><br></br>
+      </div>
+
+      
     </div>
   );
 }
